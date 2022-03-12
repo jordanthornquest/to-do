221e122afcb1045df35ab960e4ddb933869133eb
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2022-03-11 22:31:47 -0700

    grab data from fauna

--- pages/api/list.js
+++ pages/api/list.js
@@ -1,2 +1,7 @@
-export default async (req, res) => {
+import { listToDos } from '../../lib/fauna'
+
+export default async function handler(req, res) {
+  const entries = await listToDos()
+
+  return res.json(entries)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
+// Import SWR for client-side data loading
+import useSWR from 'swr'
+
+// Import components
 import Head from 'next/head'
 import { Header } from '../components/Header/'
 
-export default function Home() {
+// Create fetcher for API queries
+const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+// Create page
+export default function ToDos() {
+  // Use SWR and fetcher to ping API for entries
+  const { data, error } = useSWR('/api/list', fetcher)
+
+  // Return message on API failure
+  if (error) return <div>failed to load</div>
+
+  // Show loading message while getting data
+  if (!data) return <div>loading...</div>
+
+  // Return list of todos
   return (
     <div>
       <Head>
@@ -13,6 +31,7 @@ export default function Home() {
       <Header />
 
       <main>
+        {data.name}
       </main>
     </div>
   )
