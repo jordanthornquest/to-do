1c2167f3f7ee477926a1ae9f406abf11a59143e7
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2022-03-13 22:18:04 -0600

    refactor all todos and add styles

--- components/AllTodos/index.js
+++ components/AllTodos/index.js
@@ -4,30 +4,43 @@ import useSWR from 'swr'
 // Import components
 import { TaskItem } from '../TaskItem/'
 
+// Import styles
+import styles from './AllTodos.module.css'
+
 // Create component
 export function AllTodos() {
   // Get data from our API with SWR
   const { data, error } = useSWR('/api/list')
 
   // If there's an error, notify
-  if (error) return <p>{error}</p>
-
-  // Show loading while getting data
-  if (!data) return <p>Loading...</p>
-
-  // Destructure all todos from data
-  const { allTodos: { data: todos=undefined } } = data
+  if (error) return (
+    <main className={styles.wrapper}>
+      <h2 className={styles.heading}>Data error</h2>
+      <p className={styles.error}>{error}</p>
+    </main>
+  )
 
   // Return list of todos
   return (
-    <main>
-      {todos
+    <main className={styles.wrapper}>
+      <h2 className={styles.heading}>Active todos</h2>
+
+      {data
+        // Load todos if data is defined
         ?
-          <ul>
-            {todos.map(task => <TaskItem completed={task.completed} key={task._id} name={task.name} />)}
+          <ul className={styles.list}>
+            {data.map(task => (
+              <TaskItem
+                completed={task.done}
+                key={task._id} 
+                name={task.name}
+                taskId={task._id}
+              />
+            ))}
           </ul>
+        // Else show loading indicator
         :
-          <p>No todos!</p>
+          <p className={styles.loading}>Loading</p>
       }
     </main>
   )
