ea60ec397bf8cac023b8487a2d05a34faea03962
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2022-03-13 22:18:36 -0600

    add TaskItem checkbox interactivity and styles

--- components/TaskItem/index.js
+++ components/TaskItem/index.js
@@ -1,9 +1,40 @@
+// Import SWR
+import { useSWRConfig } from 'swr'
+
+// Import styles
+import styles from './TaskItem.module.css'
+
+// Handle task completion
+const handleTaskCompletion = async (checked, taskId) => {
+  // Set the task's completed state based on checkbox value
+  // This is a boolean value
+  const status = checked
+
+  // Send the update to the database via the API
+  // Save the response, which should be the outputted data
+  const updatedTask = await fetch(`/api/update/${taskId}`, {
+    method: 'PATCH',
+    body: JSON.stringify({completed: status})
+  })
+}
+
 // Export component
-export function TaskItem({ name, completed }) {
+export function TaskItem({ completed, taskId, name }) {
+  // Use SWR to handle mutation
+  const { mutate } = useSWRConfig()
+
+  // Render component
   return (
-    <li>
-      <input type="checkbox" checked={completed} />
-      <label>{name}</label>
+    <li className={styles.task}>
+      <div className={styles.checkboxWrapper}>
+        <input
+          checked={completed}
+          className={styles.checkbox}
+          onChange={e => mutate('/api/list', handleTaskCompletion(e.target.checked, taskId))}
+          type="checkbox"
+        />
+        <label className={styles.label}>{name}</label>
+      </div>
     </li>
   )
 }
