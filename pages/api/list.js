1d3124cfd53653e2b498c6dc6ee835604e99f32d
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2022-03-13 22:17:25 -0600

    refactor list api

--- pages/api/list.js
+++ pages/api/list.js
@@ -1,6 +1,6 @@
 // We use our own API to mask requests to Fauna
 // This is for security, and to avoid exposing environment variables
-//
+
 // Import components from library
 import { findAllTodos, graphFetcher } from '../../lib/fauna'
 
@@ -9,6 +9,9 @@ export default async function handler(req, res) {
   // Get all data with GraphQL
   const response = await graphFetcher(findAllTodos)
 
+  // Destructure all todos from data
+  const { allTodos: { data } } = response
+
   // Return response as API response
-  return res.json(response)
+  return res.json(data)
 }
